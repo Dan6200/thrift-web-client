5eedfa1bc4886de2f37050fc1637bcf1fd8135e1
Daniel Nyong <danielnyong66@gmail.com>
2023-07-03 23:33:09 +0100

    Refactored to RSC

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,38 +1,50 @@
 // cspell:ignore CldImage, cloudinary, cloudinary's
 // Purpose: Page for displaying all products
-'use client'
 import Link from 'next/link'
-import { CldImage } from 'next-cloudinary'
-import { getProducts } from './get-products'
+import { ProductImage } from '../../components/products/image'
+import axios from 'axios'
+export interface Product {
+  product_id: number
+  title: string
+  category: string
+  description: string[]
+  list_price: number
+  net_price: number
+  quantity_available: number
+  media: { filename: string; description: string }[]
+}
 
 export default async function Products() {
-  const products = await getProducts()
-  console.log(products[1])
-  return (
-    <div className="container mx-auto my-20 w-80">
-      <h2 className="w-full mx-auto my-16 text-2xl font-bold text-center">
-        Products
-      </h2>
-      <div className="flex flex-col w-full mx-auto">
-        {products.map((product) => (
-          <div className="w-full mx-auto my-4" key={product.product_id}>
-            <CldImage
-              src={product.media[0].filename}
-              className="object-cover w-full h-64"
-              width={600}
-              height={600}
-              alt={product.media[0].description}
-            />
-            <Link
-              href={`/products/${product.product_id}`}
-              passHref
-              className="inline-block w-full text-center text-blue-500"
-            >
-              {product.title.slice(0, 30) + '...'}
-            </Link>
-          </div>
-        ))}
+  try {
+    const response = await axios.get(
+      'https://thrift-dev.up.railway.app/v1/public/products?offset=0&limit=100'
+    )
+    const products = (await response.data) as Product[]
+    return (
+      <div className="container mx-auto my-20 w-80">
+        <h2 className="w-full mx-auto my-16 text-2xl font-bold text-center">
+          Products
+        </h2>
+        <div className="flex flex-col w-full mx-auto">
+          {products.map((product) => (
+            <div className="w-full mx-auto my-4" key={product.product_id}>
+              <ProductImage
+                src={product.media[0].filename}
+                alt={product.media[0].description}
+              />
+              <Link
+                href={`/products/${product.product_id}`}
+                passHref
+                className="inline-block w-full text-center text-blue-500"
+              >
+                {product.title.slice(0, 30) + '...'}
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  )
+    )
+  } catch (err) {
+    throw err
+  }
 }
---
+++ components/products/image.tsx
@@ -0,0 +1,15 @@
+//cspell:ignore CldImage, cloudinary, cloudinary's
+'use client'
+import { CldImage } from 'next-cloudinary'
+
+export function ProductImage({ src, alt }: { src: string; alt: string }) {
+  return (
+    <CldImage
+      src={src}
+      className="object-cover w-full h-64"
+      width={600}
+      height={600}
+      alt={alt}
+    />
+  )
+}
