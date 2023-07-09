f8f89cb34630904bdf2763f094ae65bd53ffeb49
Daniel Nyong <danielnyong66@gmail.com>
2023-07-09 10:16:14 +0100

    remove limit on number of products

--- app/products/get-products.ts
+++ app/products/get-products.ts
@@ -5,7 +5,7 @@ export const revalidate = 30 * 60
 
 export default async function getProducts() {
   const response = await axios.get(
-    'https://thrift-dev.up.railway.app/v1/public/products?limit=50'
+    'https://thrift-dev.up.railway.app/v1/public/products'
   )
   return (await response.data) as Product[]
 }
