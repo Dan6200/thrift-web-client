9deeab87bead942f5164a969482834b53bbcfda8
Daniel Nyong <danielnyong66@gmail.com>
2023-08-06 14:01:46 +0100

    calendar comp complete

--- components/form/register/form-fields/dob.tsx
+++ components/form/register/form-fields/dob.tsx
@@ -158,8 +158,9 @@ export const DOB = ({
               </Select>
               <Calendar
                 mode="single"
-                selected={field.value}
                 month={date}
+                onMonthChange={setDate}
+                selected={field.value}
                 onSelect={field.onChange}
                 disabled={(date) =>
                   date > new Date() || date < new Date('1900-01-01')
