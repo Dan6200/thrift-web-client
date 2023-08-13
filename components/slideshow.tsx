1a94df563f6aaabc9effa7e9602ea8a3006131b2
Daniel Nyong <danielnyong66@gmail.com>
2023-08-13 21:31:53 +0100

    updated slideshow images

--- components/slideshow.tsx
+++ components/slideshow.tsx
@@ -9,27 +9,29 @@ import { isSmallScreenAtom } from '@/atoms'
 export function Slideshow(props: PropsWithChildren) {
   const isSmallScreen = useAtomValue(isSmallScreenAtom)
   const images = [
-    { src: '/lenovo-thinkpad-t-series.jpg', alt: 'Lenovo laptop' },
-    { src: '/amazon-img.jpeg', alt: 'Wedding outfit' },
-    { src: '/L1M_DT_HERO._SX3000_QL85_FMpng_.png', alt: 'Summer wear' },
+    { src: '/pexels-ovan-62689.jpg', alt: '' },
+    { src: '/pexels-elvis-2528118.jpg', alt: '' },
+    { src: '/pexels-athena-2043590.jpg', alt: '' },
+    { src: '/pexels-pixabay-159376.jpg', alt: '' },
+    { src: '/pexels-pixabay-459762.jpg', alt: '' },
+    { src: '/pexels-harsh-kushwaha-1721558.jpg', alt: '' },
+    { src: '/pexels-noah-erickson-404280.jpg', alt: '' },
   ]
-  if (!isSmallScreen)
-    return (
-      <Carousel className="h-[32rem] shadow-sm mb-8">
-        {images.map(({ src, alt }: any) => (
-          <Link key={src} href="/products">
-            <Image
-              key={src}
-              src={src}
-              alt={alt}
-              width={1200}
-              height={600}
-              className="object-cover object-center w-full h-[30rem]"
-              {...props}
-            />
-          </Link>
-        ))}
-      </Carousel>
-    )
-  return null
+  return (
+    <Carousel className="h-96 md:h-[30rem] md:w-[80vw] mx-auto shadow-sm mb-8">
+      {images.map(({ src, alt }: any) => (
+        <Link key={src} href="/products">
+          <Image
+            key={src}
+            src={src}
+            alt={alt}
+            width={isSmallScreen ? 800 : 1000}
+            height={isSmallScreen ? 400 : 800}
+            className="object-cover object-center w-full h-80 md:h-[27rem]"
+            {...props}
+          />
+        </Link>
+      ))}
+    </Carousel>
+  )
 }
