b414b927e3126c10e119b071da5e91125e8c89ec
Daniel Nyong <danielnyong66@gmail.com>
2023-09-08 22:07:11 +0100

    Customizing search box

--- components/nav/utils/nav-menu.tsx
+++ components/nav/utils/nav-menu.tsx
@@ -103,7 +103,7 @@ export function NavMenu({
           </NavigationMenuItem>
         </NavigationMenuList>
       </div>
-      {/* <Search /> */}
+      <Search />
       <div className="flex justify-between items-center w-48">
         <div className="relative h-12 w-12 p-0">
           {!!totalItems && (
--- components/search/index.tsx
+++ components/search/index.tsx
@@ -1,19 +1,60 @@
-import React from 'react'
-import { InstantSearch, SearchBox, Hits, Highlight } from 'react-instantsearch'
+import React, { useEffect, useRef, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import {
+  InstantSearch,
+  SearchBox,
+  Hits,
+  Highlight,
+  Snippet,
+} from 'react-instantsearch'
 import { instantMeiliSearch } from '@meilisearch/instant-meilisearch'
 
-const searchClient = instantMeiliSearch(
-  'https://ms-adf78ae33284-106.lon.meilisearch.io',
-  'a63da4928426f12639e19d62886f621130f3fa9ff3c7534c5d179f0f51c4f303'
-)
+const searchClient = instantMeiliSearch('https://thrift-search.onrender.com')
 
-const Search = () => (
-  <InstantSearch indexName="steam-video-games" searchClient={searchClient}>
-    <SearchBox />
-    <Hits hitComponent={Hit} />
-  </InstantSearch>
-)
+const Search = () => {
+  const [show, setShow] = useState(false)
+  const searchRef = useRef<HTMLDivElement | null>(null)
+  useEffect(() => {
+    document.addEventListener('mousedown', hide)
+    return () => document.addEventListener('mousedown', hide)
+  }, [])
+  const hide = (e: Event) => {
+    if (searchRef.current && searchRef.current !== e.target) {
+      setShow(false)
+    }
+  }
+  return (
+    <InstantSearch indexName="products" searchClient={searchClient}>
+      <div className="absolute top-0 flex flex-col z-1000 items-center  mt-4 left-[50%] translate-x-[-50%]">
+        <span ref={searchRef}>
+          <SearchBox onClick={() => setShow(true)} />
+        </span>
+        <Hits
+          hitComponent={Hit}
+          style={{ display: show ? 'block' : 'none' }}
+          onClick={() => setShow(false)}
+          className="p-8 border relative top-0 rounded-md w-[80vw] h-[80vh] bg-background overflow-y-scroll"
+        />
+      </div>
+    </InstantSearch>
+  )
+}
 
-const Hit = ({ hit }: { hit: any }) => <Highlight attribute="name" hit={hit} />
+const Hit = ({ hit }: { hit: any }) => {
+  const router = useRouter()
+  return (
+    <article
+      className="my-4 p-2 rounded-md hover:bg-foreground/10"
+      onClick={() => {
+        router.push(`/products/${hit.product_id}`)
+      }}
+    >
+      <h1 className="font-bold text-lg">
+        <Highlight attribute="title" hit={hit} key={hit.product_id} />
+      </h1>
+      <Snippet attribute="description" hit={hit} key={hit.product_id} />
+    </article>
+  )
+}
 
 export default Search
